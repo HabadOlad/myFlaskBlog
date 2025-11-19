4de1cffd77edd7eb10212da9bce381c3afb80bc3
Habad Olad <olad.hab@gmail.com>
2025-11-19 09:21:13 +0000

    feat:the skeleton of the blog

---
+++ app.py
@@ -0,0 +1,3 @@
+#this project uses flask-login,Flask-SQLAlchemy and flask , remember for the requirements txt! 
+from flask import Flask, render_template
+
---
+++ website/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+def create_app():
+    app = Flask(__name__) #this references the module that we are going to use to run this app
+    app.config['SECRET_KEY'] = "habadolad"
+
